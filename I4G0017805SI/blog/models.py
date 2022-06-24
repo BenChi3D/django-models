ca3fa545f4798a11f77ec36f74d12a725acd5805
BenChi3D <benediktchima@gmail.com>
2022-06-24 07:57:09 +0100

    updated foreign key

--- I4G0017805SI/blog/models.py
+++ I4G0017805SI/blog/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Post(models.Model):
     Title = models.CharField(max_length=200)
     Text = models.TextField()
+    author = models.ForeignKey(
+            get_user_model(), null=True,
+            on_delete=models.CASCADE)
     Created_date = models.DateTimeField()
     Published_date = models.DateTimeField()
